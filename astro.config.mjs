8b387429d6697200b574c76f5c6f07344fc6e94e
Aleks Petrov <hello@alekspetrov.com>
2022-10-26 14:39:08 +0200

    cleanup initial and initial deploy to Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 
-import sitemap from '@astrojs/sitemap';
+// https://astro.build/config
+import tailwind from "@astrojs/tailwind";
+
+// https://astro.build/config
+import image from "@astrojs/image";
+
+// https://astro.build/config
+import prefetch from "@astrojs/prefetch";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
+  site: "https://example.com",
+  integrations: [mdx(), sitemap(), tailwind(), image(), prefetch()],
 });
