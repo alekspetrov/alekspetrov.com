c7c2f5856a99a6a13d31b189114eb2ad58d05844
Aleks Petrov <hello@alekspetrov.com>
2022-11-06 02:35:24 +0100

    upd: content nad text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,14 +10,14 @@ module.exports = {
       colors: {
         light: {
           title: "#121113",
-          text: "#8f9292",
+          text: "#67626A",
           bg: "#f5f4f5",
           outline: "#e8e8e9",
           card: "#ffffff",
         },
         dark: {
           title: "#e7e8e8",
-          text: "#8f9292",
+          text: "#9F9CA0",
           bg: "#1a181b",
           outline: "#29282a",
           card: "#1f1d20",
