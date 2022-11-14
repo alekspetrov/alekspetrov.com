d8861f4a97166f5c869f9a8cb6229bd2da282f7f
Aleks Petrov <hello@alekspetrov.com>
2022-11-14 10:38:29 +0100

    remove pnpm-lock before cleanup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,12 @@
 import { defineConfig } from "astro/config";
+
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
-
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
-
-// https://astro.build/config
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import prefetch from "@astrojs/prefetch";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://alekspetrov.com",
   integrations: [mdx(), sitemap(), tailwind(), image(), prefetch()],
 });
