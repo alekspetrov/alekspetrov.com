3d21d55ff3e096f0748e7eed81ed0ae7b28c9983
Aleks Petrov <hello@alekspetrov.com>
2022-11-21 11:12:54 +0100

    update anilytics data binding

--- src/scripts/bee.ts
+++ src/scripts/bee.ts
@@ -5,12 +5,18 @@ splitbee.init({
   apiUrl: "/_hive",
 });
 
-export function trackSubscription(email) {
+export function trackSubscription(email: string): void {
+  splitbee.user.set({ email: email });
+  splitbee.user.set({ source: "alekspetrov.com" });
+
   splitbee.track("Subscribe", {
     email: email,
   });
 }
 
-export function trackServerIssues(email, error) {
-  splitbee.track("Server Error", { email: email, error: error });
+export function trackServerIssues(email: string, error: any): void {
+  splitbee.track("Server Error", {
+    email: email,
+    error: error,
+  });
 }
