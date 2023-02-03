381c31e1a9004f1df0b6e03e0c06c92544c0ef86
Aleks Petrov <hello@alekspetrov.com>
2023-02-03 11:05:26 +0100

    progress

---
+++ src/content/config.ts
@@ -0,0 +1,26 @@
+import { z, defineCollection } from "astro:content";
+
+const blogCollection = defineCollection({
+  schema: z.object({
+    isDraft: z.boolean(),
+    isFeatured: z.boolean(),
+    layout: z.literal("../../layouts/ArticleLayout.astro"),
+    title: z.string(),
+    tags: z.array(z.string()),
+    image: z
+      .object({
+        src: z.string(),
+        alt: z.string(),
+      })
+      .optional(),
+    openGraph: z.object({
+      image: z.string(),
+      description: z.string(),
+    }),
+    publishDate: z.date(),
+  }),
+});
+
+export const collections = {
+  blog: blogCollection,
+};
