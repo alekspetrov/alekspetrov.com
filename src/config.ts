562b15175a42f73c771b77dcab912776ae7d7376
Aleks Petrov <hello@alekspetrov.com>
2023-03-02 14:19:32 +0100

    upd

--- src/config.ts
+++ src/config.ts
@@ -3,13 +3,14 @@
 
 export const SITE_URL = "https://alekspetrov.com";
 export const SITE_TITLE = "Aleks Petrov";
-export const SITE_OG_IMAGE_URL =
+export const OG_DEFAULT_DESCRIPTION = "Product Designer & Developer";
+export const OG_DEFAULT_IMAGE =
   "https://alekspetrov.com/images/og_default_image.png";
 export const CURRENT_LOCATION = "Central EU (GMT+1)";
-export const SITE_DESCRIPTION = "Product Designer & Maker";
+
 export const EMAIL = "hello@alekspetrov.com";
-export const TWITTER = "https://twitter.com/aleks_maker";
-export const LINKEDIN = "https://www.linkedin.com/in/alekspetrov";
+export const TWITTER_URL = "https://twitter.com/aleks_maker";
+export const LINKEDIN_URL = "https://www.linkedin.com/in/alekspetrov";
 export const SUPABASE_PUBLIC_URL = "https://eathcybqadpvaknyrpsb.supabase.co";
 export const SUPABASE_PUBLIC_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhdGhjeWJxYWRwdmFrbnlycHNiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc0OTAzMTMsImV4cCI6MTk4MzA2NjMxM30.zeWfDRE_mRWl3KnrcBvbHv_GLfU3fcOFy7yJj0hzFiM";
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,38 +5,80 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ["ClashDisplay-Variable", "system-ui", "sans-serif"],
-        "sans-2": ["Open Sans", "system-ui", "sans-serif"],
+        sans: ["Open Sauce", "system-ui", "sans-serif"],
       },
-      maxWidth: {},
-      colors: {
-        graphite: {
-          900: "#090809",
-          800: "#121212",
-          700: "#181818",
-          600: "#1F1F1F",
-          500: "#333333",
-          450: "#757575",
-          400: "#A0A0A0",
-          300: "#BDBDBD",
-          200: "#D9D9D9",
-          150: "F0F0F0",
-          100: "#F8F8F8",
-        },
-        brand: {
-          indigo: "#7C00AC",
-          pink: "#D7009B",
-          success: "#00D796",
-          warning: "#F5A623",
-          alert: "#D74D00",
-        },
-        social: {
-          twitter: "#1DA1F2",
-          linkedin: "#0A66C2",
-        },
+    },
+    colors: {
+      black: "#090809",
+      white: "#ffffff",
+      gray: {
+        900: "#1D1F20",
+        800: "#353739",
+        700: "#5F6061",
+        600: "#7B7B7B",
+        500: "#9D9D9D",
+        400: "#B5B5B5",
+        300: "#D5D5D5",
+        200: "#E5E6E8",
+        100: "#F4F5F5",
       },
-      lineHeight: {},
-      backgroundImage: {},
+      social: {
+        twitter: "#1DA1F2",
+        linkedin: "#0A66C2",
+      },
+    },
+    backgroundImage: {
+      highlight: "url('/images/bg_highlights.png'), #1d1f20",
+    },
+    fontSize: {
+      huge: [
+        "5.5rem",
+        {
+          lineHeight: "1em",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+      large: [
+        "4rem",
+        {
+          lineHeight: "1.1em",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+      medium: [
+        "2rem",
+        {
+          lineHeight: "1.1em",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+      title: [
+        "1.5rem",
+        {
+          lineHeight: "1.5em",
+          letterSpacing: "-0.01em",
+          fontWeight: "700",
+        },
+      ],
+      body: [
+        "1.125rem",
+        {
+          lineHeight: "1.5em",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
+      small: [
+        "1rem",
+        {
+          lineHeight: "1.5em",
+          letterSpacing: "0",
+          fontWeight: "400",
+        },
+      ],
     },
   },
   plugins: [],
