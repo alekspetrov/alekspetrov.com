de72f9a50ca3ddbd80ef643348bb4dca9ab2851d
Aleks Petrov <hello@alekspetrov.com>
2023-03-03 06:52:02 +0100

    upd

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,10 +1,10 @@
 import rss from "@astrojs/rss";
-import { SITE_TITLE, SITE_DESCRIPTION, SITE_URL } from "../config";
+import { SITE_TITLE, OG_DEFAULT_DESCRIPTION, SITE_URL } from "../config";
 
 export const get = () =>
   rss({
     title: SITE_TITLE,
-    description: SITE_DESCRIPTION,
+    description: OG_DEFAULT_DESCRIPTION,
     site: SITE_URL,
     items: import.meta.glob("./articles/**/*.{md,mdx}"),
     customData: `<language>en-us</language>`,
