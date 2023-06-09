796746e6ad589efbd57f008ceaa1685124038946
Aleks Petrov <hello@alekspetrov.com>
2023-06-09 12:18:48 +0200

    add google analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,19 @@
 import { SITE_URL } from "./src/config";
 import { defineConfig } from "astro/config";
-
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
 import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
+// https://astro.build/config
 export default defineConfig({
   site: SITE_URL,
-  integrations: [
-    mdx(),
-    tailwind(),
-    sitemap({
-      filter: (page) => page !== "https://alekspetrov.com/work-experience/",
-    }),
-    image(),
-    prefetch(),
-  ],
+  integrations: [mdx(), tailwind(), sitemap({
+    filter: page => page !== "https://alekspetrov.com/work-experience/"
+  }), image(), prefetch(), partytown()],
   markdown: {
-    syntaxHighlight: "prism",
-  },
+    syntaxHighlight: "prism"
+  }
 });
